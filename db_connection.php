2addc38c84591dd5e2fce7cff1485eeb973ff2ba
ChristopherV <Chris.Votoe.Official@gmail.com>
2025-02-06 17:49:53 -0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "localhost";
+$server = "44.200.18.104";
 $username = "poosd_database_commander";
 $password = "Bubblesort101";
 $database = "poosd_contact_manager";
 
 // Create a connection
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($server, $username, $password, $database);
 
 // Check if the connection is successful
 if ($conn->connect_error) {
