46d8c8d4fc75fae9b3d2275524132c4f87dd4ee1
Leah <leaflem@hotmail.com>
2025-02-07 18:34:09 -0500

    Update AddUser.php

--- AddUser.php
+++ AddUser.php
@@ -1,19 +1,19 @@
 <?php
-  $indata = getRequestInfo();
+	$indata = getRequestInfo();
 
 	// These grab the information by their labels in the json
-	// TODO: Not sure what they would be named in the json so I just used their names from the database as a placeholder
-  $userId = $indata["user_id"];
+	// TODO: Not sure what these would be named in the json so I just used their names from the database as a placeholder
+	$userId = $indata["user_id"];
 	$login = $indata["user_login"];
 	$password = $indata["user_password"];
 	$email = $indata["user_email"];
 	$company = $indata["user_company"];
 	$phone = $indata["user_phone_number"];
 	$firstName = $indata["first_name"];
-  $lastName = $indata["last_name"];
+	$lastName = $indata["last_name"];
 	$bday = $indata["birth_date"];
 
-  $conn = new mysqli("localhost", "poosd_database_commander", "Bubblesort101", "poosd_contact_manager");
+	$conn = new mysqli("localhost", "poosd_database_commander", "Bubblesort101", "poosd_contact_manager");
 	if ($conn->connect_error) {
 		returnWithError($conn->connect_error);
 	} 
@@ -21,13 +21,13 @@
 	else {
 		$stmt = $conn->prepare("INSERT into Users (user_id,user_login,user_password,user_email,first_name,last_name,birth_date,user_company,user_phone_number) VALUES(?,?,?,?,?,?,?,?,?)");
 		$stmt->bind_param("sssssssss", $userId,$login,$password,$email,$firstName,$lastName,$bday,$company,$phone);
-		$stmt->execute();  // Sends the sql statement to the database
+		$stmt->execute();
 		$stmt->close();
 		$conn->close();
 		returnWithError("");
 	}
 
-  function getRequestInfo() {
+    	function getRequestInfo() {
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
