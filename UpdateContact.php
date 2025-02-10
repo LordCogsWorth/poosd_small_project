b2f617eabc45d1ebf9d2bf508dd9a314cdb9babd
Megan Bailey <mbailey0811@yahoo.com>
2025-02-10 17:12:17 -0500

    implement ability to update fields individually

--- UpdateContact.php
+++ UpdateContact.php
@@ -15,8 +15,51 @@
     }
     else
     {
-        $stmt = $conn->prepare("UPDATE contacts SET first_name=?, last_name=?, contact_email=?, contact_phone_number=?, contact_company=? WHERE contact_id=? AND user_id=?");
-        $stmt->bind_param("sssssii", $inData["firstName"], $inData["lastName"], $inData["email"], $inData["phone"], $inData["company"], $inData["id"], $inData["userId"]);
+        //They may only try to update a few fields. Make sure we
+        //check for empty fields before sending a bad query
+        $fieldsToUpdate = [];
+        $stmtstmtParams = [];
+        $datatypes = "";
+
+        if (!empty($inData["firstName"])) {
+            $fieldsToUpdate[] = "first_name = ?";
+            $stmtParams[] = $inData["firstName"];
+            $datatypes .= "s";
+        }
+        if (!empty($inData["lastName"])) {
+            $fieldsToUpdate[] = "last_name = ?";
+            $stmtParams[] = $inData["lastName"];
+            $datatypes .= "s";
+        }
+        if (!empty($inData["email"])) {
+            $fieldsToUpdate[] = "contact_email = ?";
+            $stmtParams[] = $inData["email"];
+            $datatypes .= "s";
+        }
+        if (!empty($inData["phone"])) {
+            $fieldsToUpdate[] = "contact_phone_number = ?";
+            $stmtParams[] = $inData["phone"];
+            $datatypes .= "s";
+        }
+        if (!empty($inData["company"])) {
+            $fieldsToUpdate[] = "contact_company = ?";
+            $stmtParams[] = $inData["company"];
+            $datatypes .= "s";
+        }
+
+        if (empty($fieldsToUpdate)) {
+            returnWithError("No fields provided to update.");
+            $conn->close();
+            exit();
+        }
+
+        // Add contact ID and user ID for WHERE clause
+        $stmtParams[] = $inData["id"];
+        $stmtParams[] = $inData["userId"];
+        $datatypes .= "ii";
+
+        $stmt = $conn->prepare("UPDATE contacts SET " . implode(", ", $fieldsToUpdate) . " WHERE contact_id = ? AND user_id = ?");
+        $stmt->bind_param($datatypes, ...$stmtParams);
 
         if ($stmt->execute()) 
         {
@@ -26,7 +69,7 @@
             } 
             else 
             {
-                returnWithError("No records updated. Check if the contact ID and UserID are correct.");
+                returnWithError("No records updated.");
             }
         } 
         else 
